022d3a27c057dfbab02c703ac3ef4a9e9c8cd62b
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2020-12-01 16:14:57 +0530

    Create App.js

---
+++ Projects/Todo-App/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+// import TodoForm from './Todo-App/components/TodoForm';
+import TodoList from './Todo-App/components/TodoList';
+// import Todo from './Todo-App/components/Todo'
+function App() {
+  return (
+    <div className="App">
+   <h1> I  am bhubesh</h1>
+   <TodoList></TodoList>
+   {/* <Todo></Todo> */}
+    </div>
+  );
+}
+
+export default App;
